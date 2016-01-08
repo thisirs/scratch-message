a6abbe40a27401ae173e3c72246fc22d044291c0
thisirs <thisirs@gmail.com>
2016-01-08 11:14:51 +0100

    Update copyright years

--- scratch-message.el
+++ scratch-message.el
@@ -1,6 +1,6 @@
 ;;; scratch-message.el --- Changing message in your scratch buffer
 
-;; Copyright (C) 2013-2015 Sylvain Rousseau
+;; Copyright (C) 2013-2016 Sylvain Rousseau
 
 ;; Author: Sylvain Rousseau <thisirs at gmail dot com>
 ;; Maintainer: Sylvain Rousseau <thisirs at gmail dot com>
