9c7a1018e4c03a1b9489cb402518ab38d3472a6f
thisirs <thisirs@gmail.com>
2016-03-20 18:23:28 +0100

    Fix `with-demoted-errors' wrong number of arguments

--- scratch-message.el
+++ scratch-message.el
@@ -96,7 +96,7 @@ try."
                    (and (let ((ts (buffer-local-value 'buffer-display-time
                                                       (get-buffer "*scratch*"))))
                           (and ts (time-less-p scratch-message-timestamp ts))))))
-      (with-demoted-errors
+      (with-demoted-errors "Error: %S"
           (funcall scratch-message-function))
       (setq scratch-message-timestamp (current-time)))
     (setq scratch-message-timer (run-with-timer
